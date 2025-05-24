280ad1f7af0e442c1acad41eef2e3ea32f0ab4dc
Lakshman Mandapati <lakshmanmandapati0@gmail.com>
2025-05-24 12:04:00 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/todo-master/',
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+export default defineConfig(({ mode }) => ({
+  base: "/todo-master",
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
-});
+}));
